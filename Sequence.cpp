b7a8e3c915f1ffe4dbe01d15d42f9942a52d8c40
Prince Patel <princepatel.vis15@gmail.com>
2025-10-10 23:02:56 -0400

    Made erase, front and back

--- Sequence.cpp
+++ Sequence.cpp
@@ -15,7 +15,6 @@ Sequence::Sequence(size_t sz) {
         node1 = new SequenceNode;
         head = node1;
         SequenceNode *current = head;
-        head = node1;
         numElts = 1;
 
         size_t count = 1;
@@ -35,29 +34,17 @@ Sequence::Sequence(const Sequence &s) : numElts(s.numElts), head(s.head), tail(s
     SequenceNode *current = s.head;
 
     while (current) {
-        addNode(current->item);
+        push_back(current->item);
         current = current->next;
     }
 }
 
-void Sequence::addNode(string item) {
-    SequenceNode *newNode = new SequenceNode;
 
-    newNode->item = item;
-    newNode->next = nullptr;
-    newNode->prev = tail;
-
-    if (this->head == nullptr) {
-        this->head = this->tail = newNode;
-    } else {
-        this->tail->next = newNode;
-        this->tail = newNode;
-    }
-
-    numElts++;
+std::string &Sequence::operator[](size_t position) {
 }
 
-void Sequence::clearlist() {
+
+void Sequence::clear() {
     SequenceNode *current = head;
     while (current != nullptr) {
         SequenceNode *temp = current->next;
@@ -70,11 +57,70 @@ void Sequence::clearlist() {
     head = nullptr;
 }
 
-
-Sequence::~Sequence() {
-    clearlist();
+void Sequence::erase(size_t position) {
+    SequenceNode *current = head;
+    if (current == nullptr) {
+        return;
+    }else {
+        size_t count = 0;
+        while (count < position) {
+            current = current->next;
+            count++;
+        }
+        current -> prev -> next = current-> next;
+        current -> prev -> prev = current ->prev;
+        delete current;
+    }
+    numElts--;
 }
 
+void Sequence::erase(size_t position, size_t count) {
+    SequenceNode *current = head;
+    if (current == nullptr) {
+        return;
+    }else{
+        size_t length = 0;
+        while (length < position) {
+            current = current->next;
+            length++;
+        }
+        while (count != 0) {
+            SequenceNode *temp = current;
+            current = current->next;
+            temp -> prev -> next = temp-> next;
+            temp -> next -> prev = temp ->prev;
+            delete temp;
+            count--;
+            numElts--;
+        }
+
+    }
+
+}
+
+
+Sequence::~Sequence() {
+    clear();
+}
+
+Sequence & Sequence::operator=(const Sequence &s) {
+
+  clear();
+
+    SequenceNode *current = s.head;
+    while (current != nullptr) {
+        SequenceNode *temp = current;
+        current = current->next;
+        delete temp;
+    }
+
+    head = tail = nullptr;
+    numElts = 0;
+
+
+    return *this;
+
+}
 void Sequence::push_back(std::string item) {
 
     SequenceNode *newNode = new SequenceNode;
@@ -82,13 +128,28 @@ void Sequence::push_back(std::string item) {
     newNode->next = nullptr;
     newNode->prev = tail;
 
-    if (this->head == nullptr) {
-        this->head = this->tail = newNode;
+    if (head == nullptr) {
+        head = tail = newNode;
     } else {
         tail->next = newNode;
         tail = newNode;
     }
     numElts++;
+}
+
+void Sequence::pop_back() {
+    SequenceNode *current = tail;
+
+    if (numElts == 1) {
+        delete head;
+        head = nullptr;
+        tail = nullptr;
+    }else if(current->next == nullptr) {
+        tail = tail -> prev;
+        tail->next = nullptr;
+        delete current;
+    }
+    numElts--;
 }
 
 void Sequence::insert(size_t position, std::string item) {
@@ -117,11 +178,43 @@ void Sequence::insert(size_t position, std::string item) {
         }
 
         newNode->next = current->next;
-        current->prev = current;
+        newNode->prev = current;
         current->next->prev = newNode;
         current->next = newNode;
     }
     numElts++;
+}
+
+std::string Sequence::front() const {
+    SequenceNode *current = head;
+
+    if (current == nullptr) {
+        return "Empty List";
+    }else {
+        return current->item;
+    }
+}
+
+std::string Sequence::back() const {
+    SequenceNode *current = tail;
+    if (current == nullptr) {
+        return "Tail Empty";
+    }else {
+        return current->item;
+    }
+}
+
+bool Sequence::empty() const {
+    SequenceNode *current = head;
+    if (current == nullptr) {
+        return true;
+    }else {
+        return false;
+    }
+}
+
+size_t Sequence::size() const {
+    return numElts;
 }
 
 ostream & operator<<(ostream &os, const Sequence &s) {
@@ -139,5 +232,6 @@ ostream & operator<<(ostream &os, const Sequence &s) {
         current = current->next;
         count++;
     }
+    return os;
 
 }
--- SequenceDebug.cpp
+++ SequenceDebug.cpp
@@ -6,26 +6,88 @@
  * In this file, you will write your tests as you implement Sequence. If you are using CLion, you need to select
  * SequenceDebug from the drop-down menu next to the Build (hammer icon) if it is on SequenceTestHarness
  */
+#include <iomanip>
+
 #include "Sequence.h"
 #include <iostream>
+
+void testCopy();
+
+void addSomeGrades(Sequence nod);
+
+void addSomeGrades(Sequence nod) {
+    nod.push_back("p");
+    nod.push_back("a");
+    nod.push_back("b");
+
+    cout << "in addSomeGrades(nod)" << endl;
+    cout << nod << endl;
+    cout << endl;
+}
+
+void testAddGrade();
+
 
 using namespace std;
 
 int main() {
     cout << "Your project is ready to go!" << endl;
 
-    Sequence seq(1);
 
-    seq.push_back("num1");
-    seq.push_back("num2");
-    seq.push_back("num3");
-    seq.push_back("num4");
+    Sequence seq;
+    seq.push_back("p");
+    seq.insert(0, "num5");
+    seq.push_back("num6");
+    seq.push_back("num7");
+    seq.push_back("num8");
 
-    seq.insert(2, "num5");
 
     cout << seq << endl;
 
+    cout << endl;
+
+    seq.erase(1, 2);
+
+
+    cout << seq << endl;
+
+    cout << seq.front();
+
+    cout << seq.back();
+    cout << seq.empty();
+    cout << seq.size() << endl;
 
 
     return 0;
+}
+
+void testAddGrade() {
+    Sequence seq1(1);
+    cout << "In testADDGRADE" << endl;
+    cout << endl;
+
+    seq1.push_back("some");
+    seq1.insert(2, "num");
+
+    cout << seq1 << endl;
+
+    cout << "In testADDGRADE" << endl;
+    cout << endl;
+}
+
+void testCopy() {
+    Sequence testCopy(1);
+
+    testCopy.push_back("prince");
+    testCopy.push_back("1");
+
+    cout << "In testCopy" << endl;
+    cout << testCopy << endl;
+    cout << endl;
+
+    addSomeGrades(testCopy);
+
+    cout << "In testCopy" << endl;
+    cout << testCopy << endl;
+    cout << endl;
 }
